bb9d9a6351dfd806233f652900750f8843beb4a0
mincheol-creator <kimmc1030@naver.com>
2020-07-10 23:50:57 +0900

    .

--- test/test.py
+++ test/test.py
@@ -1,2 +1,11 @@
-arr=['3','30','34','5','9']
-print(sorted(arr, key=lambda x:x*3, reverse=True))
+# arr=['3','30','34','5','9']
+# print(sorted(arr, key=lambda x:x*3, reverse=True))
+
+phoneBook = ["119", "97674223", "1195524421","543151","6456415416","12371928312"]
+phoneBook = sorted(phoneBook)
+for p1,p2 in zip(phoneBook, phoneBook[1:]):
+    print(p1)
+    print(p2)
+    if p2.startswith(p1):
+        print("false")
+print("true")
