f3479c12408d96a8adfe20056b77d25cabbeef17
mincheol-creator <kimmc1030@naver.com>
2020-08-12 22:07:07 +0900

    SWEA D2

--- test/test.py
+++ test/test.py
@@ -1,19 +1,44 @@
 import sys
 sys.stdin = open('input_test.txt')
 
-Numbers=int(input())
-for Number in range(1, Numbers+1):
-    print(f'#{Number}')
-
-    Counts=int(input())
-    enter_count=0
-    for Count in range(0,Counts):
-        input_arr=[]
-        input_arr=list(input().split())
-        for i in range(int(input_arr[1])):
-            print(input_arr[0], end="")
-            enter_count+=1
-            if enter_count==10:
-                print()
-                enter_count=0
-    print()
+ts=int(input())
+for t in range(1, ts+1):
+    Ns=int(input())
+    row=1
+    col=Ns-1
+    direct=1
+    arr=[[i for i in range(1,Ns+1)]]
+    for i in range(Ns-1):
+        arr.append([0]*Ns)
+    for N in range(Ns+1,Ns*2):
+        arr[row][col]=N
+        if row!=(Ns-1):
+            row+=1
+    for N in range(Ns*2,Ns*3-1):
+        if col!=0:
+            col-=1
+        arr[row][col]=N
+    for N in range(Ns*3-1,(Ns)**2+1):
+        if direct==1:
+            if arr[row-1][col]==0:
+                arr[row-1][col]=N
+                row-=1
+            elif arr[row][col+1]==0:
+                arr[row][col+1]=N
+                col+=1
+            else:
+                direct=2
+        if direct==2:
+            if arr[row+1][col]==0:
+                arr[row+1][col]=N
+                row+=1
+            elif arr[row][col-1]==0:
+                arr[row][col-1]=N
+                col-=1
+            else:
+                direct=1
+    print(f'#{t}')
+    for i in range(Ns):
+        for j in range(Ns):
+            print(arr[i][j], end=" ")
+        print()
